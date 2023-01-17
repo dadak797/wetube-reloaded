1d066653c7a036ed3d301b0c5cf5107d10cfeae3
Park Seung-Keun <dadak797@gmail.com>
2023-01-17 14:24:17 +0900

    Chapter4

--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter.js";
+import videoRouter from "./routers/videoRouter.js";
+import userRouter from "./routers/userRouter.js";
+
+
 const PORT = 4000;
 const app = express();
+
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    return res.send("<h1>I love you.</h1>");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-}
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);

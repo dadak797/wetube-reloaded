54d3ee78251719b56da38376fdb8749fdec6cdfe
Park Seung-Keun <dadak797@gmail.com>
2023-01-17 21:52:46 +0900

    Chapter5

--- src/server.js
+++ src/server.js
@@ -7,14 +7,19 @@ import userRouter from "./routers/userRouter.js";
 
 
 const PORT = 4000;
+
 const app = express();
-
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");  // Set the view engine
+app.set("views", process.cwd() + "/src/views");  // Set the path of views
+
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+const handleListening = () => 
+    console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+
 app.listen(PORT, handleListening);

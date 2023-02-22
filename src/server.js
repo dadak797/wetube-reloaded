8032e9f2bd00433a0cff0fca36a09b67dfdad84a
Park Seung-Keun <dadak797@gmail.com>
2023-02-23 00:17:08 +0900

    Chapter7

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
-
-import globalRouter from "./routers/globalRouter.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter.js";
 import videoRouter from "./routers/videoRouter.js";
 import userRouter from "./routers/userRouter.js";
+import { localsMiddleware } from "./middlewares.js";
 
 
 const app = express();
@@ -14,7 +16,20 @@ app.set("views", process.cwd() + "/src/views");  // Set the path of views
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));  // For encoding of req.body
-app.use("/", globalRouter);
+
+// Session middleware
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 300000,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+}));
+
+app.use(localsMiddleware);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 

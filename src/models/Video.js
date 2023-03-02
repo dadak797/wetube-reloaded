0337c46d7889fa7c01cd4270b29c5be1376883a3
Park Seung-Keun <dadak797@gmail.com>
2023-03-02 16:08:22 +0900

    Chapter8

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true, maxLength: 80 },
+    fileUrl: { type: String, required: true },
     description: { type: String, required: true, trum: true, minLength: 20 },
     createdAt: { type: Date, required: true, default: Date.now },
     hashtags: [{ type: String, trim: true }],
@@ -11,6 +12,7 @@ const videoSchema = new mongoose.Schema({
         views: { type: Number, default: 0, required: true },
         rating: { type: Number, default: 0, required: true },
     },
+    owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 videoSchema.static('formatHashtags', function(hashtags) {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,37 @@
 import express from "express";
 import { 
-    edit, 
+    getEdit,
+    postEdit, 
     logout, 
     see,
     startGithubLogin,
-    finishGithubLogin
+    finishGithubLogin,
+    getChangePassword,
+    postChangePassword
 } from "../controllers/userController.js";
+import {
+    protectorMiddleware, 
+    publicOnlyMiddleware, 
+    avatarUpload
+} from "../middlewares.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter
+    .route("/edit")
+    .all(protectorMiddleware)
+    .get(getEdit)
+    .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+    .route("/change-password")
+    .all(protectorMiddleware)
+    .get(getChangePassword)
+    .post(postChangePassword);
+
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+
 userRouter.get("/:id", see);
 
 export default userRouter;

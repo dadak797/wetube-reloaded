b1fa66a0ace1bd7d4e1953dcb9a7dc81b4c84393
Park Seung-Keun <dadak797@gmail.com>
2023-03-14 09:19:28 +0900

    Chapter12

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter.js";
 import videoRouter from "./routers/videoRouter.js";
 import userRouter from "./routers/userRouter.js";
+import apiRouter from "./routers/apiRouter.js";
 import { localsMiddleware } from "./middlewares.js";
 
 
@@ -34,5 +35,6 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;

a407f206f5a84259ee24afbf2828937baba22df1
Park Seung-Keun <dadak797@gmail.com>
2023-03-27 00:03:58 +0900

    Production Environment

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,7 +13,6 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id).populate("owner").populate("comments");
-    console.log(video);
     if (!video) {
         return res.status(404).render("404", { pageTitle: "Video not found." });
     }
@@ -70,12 +69,13 @@ export const postUpload = async (req, res) => {
     } = req.session;
     const { video, thumb } = req.files;
     const { title, description, hashtags } = req.body;
+    const isHeroku = process.env.NODE_ENV === "production";
     try {
         const newVideo = await Video.create({
             title,
             description,
-            fileUrl: video[0].location,
-            thumbUrl: thumb[0].location,//.replace(/[\\]/g, "/"),
+            fileUrl: isHeroku ? video[0].location : video[0].path,
+            thumbUrl: isHeroku ? thumb[0].location : thumb[0].path,//.replace(/[\\]/g, "/"),
             owner: _id,
             hashtags: Video.formatHashtags(hashtags)
         });
@@ -109,7 +109,6 @@ export const deleteVideo = async (req, res) => {
 
 export const search = async (req, res) => {
     const { keyword } = req.query;
-    console.log(keyword);
     let videos = [];
     if (keyword) {
         videos = await Video.find({
--- src/middlewares.js
+++ src/middlewares.js
@@ -11,21 +11,35 @@ const s3 = new S3Client({
     }
 });
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
     s3: s3,
-    bucket: "daktube",
-    // acl: "public-read"
-    Condition: {
-        StringEquals: {
-        "s3:x-amz-acl": ["public-read"],
-        },
-    }
+    bucket: "daktube/images",
+    acl: "public-read"
+    // Condition: {
+    //     StringEquals: {
+    //     "s3:x-amz-acl": ["public-read"],
+    //     },
+    // }
+});
+
+const s3VideoUploader = multerS3({
+    s3: s3,
+    bucket: "daktube/videos",
+    acl: "public-read"
+    // Condition: {
+    //     StringEquals: {
+    //     "s3:x-amz-acl": ["public-read"],
+    //     },
+    // }
 });
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
+    res.locals.isHeroku = isHeroku;
     next();
 }
 
@@ -52,7 +66,7 @@ export const avatarUpload = multer({
     limits: {
         fileSize: 3000000,
     },
-    storage: multerUploader
+    storage: isHeroku ? s3ImageUploader : undefined
 });
 
 export const videoUpload = multer({
@@ -60,5 +74,5 @@ export const videoUpload = multer({
     limits: {
         fileSize: 10000000,
     },
-    storage: multerUploader
+    storage: isHeroku ? s3VideoUploader : undefined
 });
